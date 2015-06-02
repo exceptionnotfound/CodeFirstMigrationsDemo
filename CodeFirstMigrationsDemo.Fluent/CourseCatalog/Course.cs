3058634363fcfa3da76b931fd8abe3014534ac0a
exceptionnotfound <mpjones428@gmail.com>
2015-06-02 13:26:39 -0700

    Added basic classes for Fluent demo

--- CodeFirstMigrationsDemo.Fluent/CourseCatalog/Course.cs
+++ CodeFirstMigrationsDemo.Fluent/CourseCatalog/Course.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CodeFirstMigrationsDemo.CourseCatalog
+namespace CodeFirstMigrationsDemo.Fluent.CourseCatalog
 {
     public class Course
     {
--- CodeFirstMigrationsDemo.Fluent/CourseCatalog/Teacher.cs
+++ CodeFirstMigrationsDemo.Fluent/CourseCatalog/Teacher.cs
@@ -1,25 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CodeFirstMigrationsDemo.CourseCatalog
+namespace CodeFirstMigrationsDemo.Fluent.CourseCatalog
 {
     public class Teacher
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
 
-        [Required]
-        [StringLength(100)]
         public string FirstName { get; set; }
 
-        [Required]
-        [StringLength(100)]
         public string LastName { get; set; }
 
         public virtual ICollection<Course> Courses { get; set; }
